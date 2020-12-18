8d8f41de7f99fec1f5c619985355257c26b4afc5
Raymond Arias <raymond.arias@sendgrid.com>
2020-12-18 09:50:49 -0800

    Fix stuff

--- main.go
+++ main.go
@@ -23,8 +23,6 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/kubernetes-sigs/aws-iam-authenticator/pkg/token"
-
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -66,10 +64,7 @@ func getSecret(secretName string, namespace string, kubeconfig *string) {
 	if err != nil {
 		panic(err.Error())
 	}
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Println(token.Token)
+
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
